efac91b5f4d5ca27a824e26ff04d98bac7d5c846
9thprince <castpo84@gmail.com>
2019-12-17 14:37:57 +0000

    Update domain egybest

--- IPTVPlayer/hosts/hostegybest.py
+++ IPTVPlayer/hosts/hostegybest.py
@@ -39,7 +39,7 @@ def GetConfigList():
     return optionList
 ###################################################
 def gettytul():
-    return 'https://beal.egybest.xyz/'
+    return 'https://gray.egybest.me/'
 
 class EgyBest(CBaseHostClass):
     
@@ -47,7 +47,7 @@ class EgyBest(CBaseHostClass):
         CBaseHostClass.__init__(self, {'history':'egy.best', 'cookie':'egy.best.cookie'})
         self.DEFAULT_ICON_URL = 'https://ww.egybest1.com/wp-content/uploads/2019/06/icon.png'
         self.USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'
-        self.MAIN_URL = 'https://beal.egybest.xyz/'
+        self.MAIN_URL = 'https://gray.egybest.me/'
         self.HEADER = {'User-Agent': self.USER_AGENT, 'DNT':'1', 'Accept': 'text/html', 'Accept-Encoding':'gzip, deflate', 'Referer':self.getMainUrl(), 'Origin':self.getMainUrl()}
         self.AJAX_HEADER = dict(self.HEADER)
         self.AJAX_HEADER.update( {'X-Requested-With': 'XMLHttpRequest', 'Accept-Encoding':'gzip, deflate', 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 'Accept':'application/json, text/javascript, */*; q=0.01'} )
